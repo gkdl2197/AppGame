cdb0084b93473fcd621bb8c5a0e87fb3f160e01b
gkdl2197 <gkdl2197@google.com>
2019-05-14 15:58:51 +0900

    ~로딩화면 구현

--- app/src/main/java/edu/android/appgame/MainActivity.java
+++ app/src/main/java/edu/android/appgame/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.android.appgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
