2de7fcccdadc2f8cceda57679d9a34cf4ddd88bc
fly2rainbow <youmin02@gmail.com>
2019-05-24 15:18:20 +0900

    Add Asset Files

---
+++ app/src/main/java/edu/android/appgame/FacilityInfoActivity.java
@@ -0,0 +1,15 @@
+package edu.android.appgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+class FacilityInfoActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    } // end onCreate(){
+}
