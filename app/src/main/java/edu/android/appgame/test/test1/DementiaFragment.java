e79e6a97e148932f7c292fb74a506ac8959cf551
nkmj78 <seoheee96@naver.com>
2019-05-27 16:04:51 +0900

    치매 검사 1

--- app/src/main/java/edu/android/appgame/test/test1/DementiaFragment.java
+++ app/src/main/java/edu/android/appgame/test/test1/DementiaFragment.java
@@ -2,14 +2,11 @@ package edu.android.appgame.test.test1;
 
 import android.content.Context;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import edu.android.appgame.R;
@@ -55,7 +52,7 @@ public class DementiaFragment extends Fragment {
         RadioGroup.OnCheckedChangeListener onCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, final int checkedId) {
-                btnNextSurvey = getView().findViewById(R.id.btnNextSurvey);
+                btnNextSurvey = getView().findViewById(R.id.btnNext);
                 View.OnClickListener onClickListener = new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
