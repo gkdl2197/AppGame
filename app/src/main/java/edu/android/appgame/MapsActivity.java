14d462bf8039a9f9a2af8a3288026af94f84bdf7
wangji001 <giyoung001@gmail.com>
2019-05-28 18:12:09 +0900

    json 업데이트

--- app/src/main/java/edu/android/appgame/MapsActivity.java
+++ app/src/main/java/edu/android/appgame/MapsActivity.java
@@ -9,5 +9,8 @@ public class MapsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-    }
-}
+    } // end onCreate()
+
+
+} // end class MapsActivity
+
